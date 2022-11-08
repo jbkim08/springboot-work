33b9d3beb1d34ee1568f09e73d51f335944f8fe7
jbkim <drv98@naver.com>
2022-11-08 14:04:07 +0900

    질문목록 리스트

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,13 +1,22 @@
 package com.mysite.sbb.question;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class QuestionController {
+	
+	@Autowired
+	private QuestionRepository qRepo;
 
 	@RequestMapping("/question/list")
-	public String list() {
+	public String list(Model model) {
+		List<Question> qList = qRepo.findAll();
+		model.addAttribute("qList", qList);
 		return "question_list";
 	}
 }
