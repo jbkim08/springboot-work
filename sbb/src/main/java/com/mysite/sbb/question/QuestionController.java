e0a5c0712614788fc40bce6755dc14f121b8883a
jbkim <drv98@naver.com>
2022-11-09 10:39:19 +0900

    새 질문 등록

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/question")
@@ -34,4 +36,17 @@ public class QuestionController {
     public String questionCreate() {
         return "question_form";
     }
+    
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, 
+    							 @RequestParam String content) {
+        this.qService.create(subject, content);
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
+    }
 }
+
+
+
+
+
+
--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb.question;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +28,14 @@ public class QuestionService {
 			throw new DataNotFoundException("question not found");
 		}
 	}
+	// 새로운 질문을 저장
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);				//제목
+        q.setContent(content);				//내용
+        q.setCreateDate(LocalDateTime.now()); //현재시간
+        this.qRepo.save(q);			//리포지토리에 저장
+    }
 }
 
 
