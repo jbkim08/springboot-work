e9bfa23810267120ff08e2550965df8033200427
jbkim <drv98@naver.com>
2022-11-10 14:17:51 +0900

    글쓴사람 표시하기

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -5,6 +5,7 @@ import java.security.Principal;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -32,6 +33,7 @@ public class AnswerController {
 	@Autowired
 	private UserService uService;
 
+	@PreAuthorize("isAuthenticated()") 
 	@PostMapping("/create/{id}")
 	public String createAnswer(Model model, @PathVariable("id") Integer id, 
 							@Valid AnswerForm answerForm, BindingResult bindingResult,
